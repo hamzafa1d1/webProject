6550020493726469b930186a5fa05da8c456813d
hamzafa7d7 <hamza.faidi@supcom.tn>
2023-02-10 18:52:02 +0100

    added post request logic in angular for search

--- server/src/main/java/com/example/webproject/controller/searchFormController.java
+++ server/src/main/java/com/example/webproject/controller/searchFormController.java
@@ -3,20 +3,21 @@ import com.example.webproject.model.searchFormData;
 import com.example.webproject.service.searchFormDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalTime;
 import java.util.Map;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class searchFormController {
     @Autowired
     public searchFormDataService SearchFormDataService ;
     @PostMapping("/searchFormData")
     public ResponseEntity<Map<LocalTime,Object>> submitForm(@RequestBody searchFormData searchFormData){
-        System.out.println("startcity: " + searchFormData.getStartStation());
+        System.out.println("time: " + searchFormData.getTime());
         Map<LocalTime, Object> responseData = SearchFormDataService.EligeableBusses(searchFormData);
         return  ResponseEntity.ok().body(responseData) ;
 
